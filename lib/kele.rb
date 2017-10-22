7c7d401e42df9739d88f6b061db2b43ee9ab6817
brandon armand <bwelsh8@gmail.com>
2017-10-22 03:46:14 +0000

    added mentor availability method

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,9 +4,9 @@ class Kele
     include HTTParty
 
     def initialize(email, password)
-        base_url = 'https://www.bloc.io/api/v1'
+        @base_url = 'https://www.bloc.io/api/v1'
         
-        post = self.class.post('https://www.bloc.io/api/v1/sessions',
+        post = self.class.post("#{@base_url}/sessions",
             body: {
                 email: email, 
                 password: password 
@@ -21,7 +21,16 @@ class Kele
     end
     
     def get_me
-        response = self.class.get('https://www.bloc.io/api/v1/users/me', 
+        response = self.class.get("#{@base_url}/users/me", 
+            headers: { 
+                "authorization" => @auth_token 
+            })
+            
+        parsed_response = JSON.parse(response.body) 
+    end
+    
+    def get_mentor_availability(mentor_id)
+        response = self.class.get("#{@base_url}/mentors/#{mentor_id}/student_availability", 
             headers: { 
                 "authorization" => @auth_token 
             })
