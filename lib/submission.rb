98bd00be7bf290da503f5387d7c0eb870ac83b69
brandon armand <bwelsh8@gmail.com>
2017-10-22 09:40:00 +0000

    added update submission method

--- lib/submission.rb
+++ lib/submission.rb
@@ -1,8 +1,24 @@
 module Submission
    def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
-        sender = kele_client.get_me['current_enrollment']['id']
+        sender = self.get_me['current_enrollment']['id']
         
         post = self.class.post("#{@base_url}/checkpoint_submissions",
+            body: {
+                assignment_branch: assignment_branch,
+                assignment_commit_link: assignment_commit_link,
+                checkpoint_id: checkpoint_id,
+                comment: comment,
+                enrollment_id: sender
+            },
+            headers: { 
+                "authorization" => @auth_token 
+            }) 
+   end
+   
+   def update_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, id)
+        sender = self.get_me['current_enrollment']['id']
+        
+        post = self.class.post("#{@base_url}/checkpoint_submissions/:#{id}",
             body: {
                 assignment_branch: assignment_branch,
                 assignment_commit_link: assignment_commit_link,
